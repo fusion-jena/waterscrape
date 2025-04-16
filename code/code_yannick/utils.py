ccd7adbf7206baa0f61881529159b1755d0101ac
ybrenning <yannickbrenning2@gmail.com>
2025-04-16 18:30:07 +0200

    Add utils.py

---
+++ code/code_yannick/utils.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+
+def iso_to_mysql_datetime(iso_string):
+    """
+    Function to convert BlueSky datetime format (ISO) to MySQL datetime
+    """
+    try:
+        # check if time zone ('Z') is included:
+        if iso_string.endswith('Z'):
+            # remove 'Z' und format the date, '00:00' is also UTC-time:
+            dt = datetime.fromisoformat(iso_string[:-1] + '+00:00')
+        else:
+            # parsing in case of no information about time zone:
+            dt = datetime.fromisoformat(iso_string)
+
+        # format to MySQL DATETIME:
+        mysql_datetime = dt.strftime("%Y-%m-%d %H:%M:%S")
+
+        return mysql_datetime
+
+    # catch error if sth with datetime formatting went wrong:
+    except ValueError as e:
+        print(f"Error converting iso to MySQL datetime: {e}")
+        return None
