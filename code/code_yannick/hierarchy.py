86a83e296076e944f557f65d097e22b3cc13707b
ybrenning <yannickbrenning2@gmail.com>
2025-05-12 11:50:48 +0200

    Fix hierarchy script

--- code/code_yannick/hierarchy.py
+++ code/code_yannick/hierarchy.py
@@ -1,27 +1,43 @@
+file_path = "hierarchy_Social_Media.txt"
+
 keyword_categories = [
     "natural sciences", "water and technology", "social sciences"
 ]
 
-with open("hierarchy.txt", "r") as f:
-    keyword_dict = {}
-    lines = f.readlines()
-    current_head = []
-    for line in lines:
-        current_category = [cat for cat in keyword_categories if cat in line]
-        if current_category:
-            category = current_category
-            level = 1
-            assert len(category) == 1
-            category = category[0]
-        else:
-            indentation = 0
-            for c in line:
-                indentation += 1 if c == " " or not c.isascii() else 0
-                if c.isalpha():
-                    break
-            # Highest indentation signifies keyword
-            if indentation == 16:
-                keyword = "".join(c for c in line if ord(c) < 128).strip()
-                keyword_dict[keyword] = category
 
-print(keyword_dict)
+def get_keyword_dict():
+    """
+    Reads the given keyword hierarchy into a dictionary for inference.
+
+    The dictionary contains mappings of keyword categories to lists
+    of keywords in order to automatically detect "parent" keywords.
+
+    Returns:
+        keyword_dict (dict[str, list[str]])
+    """
+    with open(file_path, "r") as f:
+        category = None
+        keyword_dict = {}
+        lines = f.readlines()
+        for line in lines:
+            current_category = [cat for cat in keyword_categories if cat in line]
+            if current_category:
+                category = current_category
+                assert len(category) == 1
+                category = category[0]
+            else:
+                indentation = 0
+                for c in line:
+                    indentation += 1 if c == " " or not c.isascii() else 0
+                    if c.isalpha():
+                        break
+                # Highest indentation signifies keyword
+                if (
+                    indentation == 12
+                    or category == "social sciences" and indentation == 8
+                ):
+                    keyword = "".join(c for c in line if ord(c) < 128).strip()
+                    # print(keyword)
+                    keyword_dict[keyword] = category
+
+    return keyword_dict
