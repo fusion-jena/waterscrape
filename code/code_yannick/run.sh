a095e97bb7054ad6eed30f855d142a1b1b0aaa06
yb63tadu <yb63tadu@studserv.uni-leipzig.de>
2026-03-09 14:54:15 +0100

    Slurm script

---
+++ code/code_yannick/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=thwic
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --time=01:00:00          # Max runtime: 1 hour
+#SBATCH --output=thwic.out.%j    # stdout log
+#SBATCH --error=thwic.err.%j     # stderr log
+
+# Load CUDA
+module load nvidia/cuda/11.7
+
+source /vast/$USER/venv/bin/activate
+
+python3 analysis.py
